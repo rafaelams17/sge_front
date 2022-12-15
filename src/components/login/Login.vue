002ec0751f3d3b6bfaf5500c1ff019cc5050831b
rafaelams17 <rafaelasantosminios@gmail.com>
2022-12-14 21:12:34 -0300

    adicionando o front

---
+++ src/components/login/Login.vue
@@ -0,0 +1,23 @@
+<template>
+    <div>
+        <h1>Login</h1>
+        <form action="">
+            <p>Nome:</p>
+            <input type="text" placeholder="Digite seu nome">
+            <p>Senha:</p>
+            <input type="password" placeholder="Digite sua senha">
+            <input type="submit" value="Enviar">
+        </form>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'Login',
+    data(){
+        return{
+
+        }
+    }
+}
+</script>
